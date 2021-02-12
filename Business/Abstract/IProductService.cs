560e4e9f6442737b110d183273a1579b88fcbc28
Oguzhan Zor <hanoguzzor@gmail.com>
2021-02-12 22:05:58 +0300

    Entity Framework entegre

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -8,6 +8,8 @@ namespace Business.Abstract
     public interface IProductService
     {
         List<Product> GatAll();
-        List<Product> GetAllByCategory(int categoryId);
+        List<Product> GetAllByCategoryId(int id);
+        List<Product> GetByUnitPrice(decimal min, decimal max);
+
     }
 }
