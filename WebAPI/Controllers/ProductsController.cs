4973f992c4ce9da2ae96a796b7ac7ad0cb34b27f
Oguzhan Zor <hanoguzzor@gmail.com>
2021-03-26 05:01:45 +0300

    category contraller eklendi

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -15,7 +15,7 @@ namespace WebAPI.Controllers
     public class ProductsController : ControllerBase
     {
         //Loosely coupled
-        IProductService _productService;
+       private IProductService _productService;
 
         public ProductsController(IProductService productService)
         {
@@ -39,6 +39,17 @@ namespace WebAPI.Controllers
         public IActionResult GetById(int id)
         {
             var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetCategoryById(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
             if (result.Success)
             {
                 return Ok(result);
